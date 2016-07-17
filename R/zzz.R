336d461fa37f5d05f3174baa0fa896cedd066f93
Gerhard Burger <burger.ga@gmail.com>
2016-07-17 13:43:26 +0200

    Add roxygen to zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,17 @@
-# Take the value from javascript and do stuff with it (for example converting it to Date object)
+#' Sets up the package when it's loaded
+#'
+#' Adds the content of www to shinyTime/, and registers an inputHandler to massage the output from
+#' JavaScript into an R structure.
+#'
+#' @importFrom shiny addResourcePath registerInputHandler
+#'
+#' @noRd
+#'
 .onLoad <- function(libname, pkgname) {
   # Add directory for static resources
   addResourcePath('shinyTime', system.file('www', package='shinyTime', mustWork = TRUE))
-  shiny::registerInputHandler('my.shiny.timeInput', function(data, ...) {
+  # Do some processing on the data we get from javascript before we pass it on to R
+  registerInputHandler('my.shiny.timeInput', function(data, ...) {
     # Replace NULL by NA (will make it numeric automatically)
     data[sapply(data, is.null)] <- NA
     # Convert to vector
@@ -10,6 +19,14 @@
   })
 }
 
+#' Cleans up when package is unloaded
+#'
+#' Should reverse the effects from .onLoad
+#'
+#' @importFrom shiny removeInputHandler
+#'
+#' @noRd
+#'
 .onUnload <- function(libpath) {
-  shiny::removeInputHandler('my.shiny.timeInput')
+  removeInputHandler('my.shiny.timeInput')
 }
