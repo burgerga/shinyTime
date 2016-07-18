c7c356a63e564e5bc3f7c03e70c7c44442663881
Gerhard Burger <burger.ga@gmail.com>
2016-07-18 10:58:38 +0200

    Document the exported functions

--- R/input-time.R
+++ R/input-time.R
@@ -1,14 +1,45 @@
 #' shinyTime: A timeInput widget for Shiny
 #'
-#' Provides a widget for inputting time in the h:m:s format.
+#' Provides a time input widget for inputting time in the %H:%M:%S format.
 #'
 #' @docType package
 #' @name shinyTime
 NULL
 
-# Inspired by https://github.com/rstudio/shiny/blob/master/R/input-daterange.R
-#' @export
+#' Create a time input
+#'
+#' Creates a time widget that consists of three separate numeric inputs for respectively the hours,
+#' minutes, and seconds. The input and output values of the time widget are instances of
+#' \code{\link{DateTimeClasses}}, these can be converted to and from character strings with
+#' \code{\link{strptime}} and \code{\link{strftime}}. For a simple example app see
+#' \code{\link{shinyTimeExample}}.
+#'
+#' @inheritParams shiny::textInput
+#' @param value The desired time value. Must be a instance of \code{\link{DateTimeClasses}}.
+#'
+#' @family shinyTime functions
+#' @seealso \code{\link{strptime}}, \code{\link{strftime}}
+#'
+#' @examples
+#' ## Only run examples in interactive R sessions
+#' if (interactive()) {
+#'
+#' ui <- fluidPage(
+#'   # Default value is 00:00:00
+#'   timeInput("time1", "Time:"),
+#'
+#'   # Set to current time
+#'   timeInput("time2", "Time:", value = Sys.time()),
+#'
+#'   # Set to custom time
+#'   timeInput("time3", "Time:", value = strptime("12:34:56", "%T"))
+#' )
+#'
+#' shinyApp(ui, server = function(input, output) { })
+#' }
+#'
 #' @importFrom htmltools tagList singleton tags
+#' @export
 timeInput <- function(inputId, label, value = NULL) {
   if(is.null(value)) value <- getDefaultTime()
   value_list <- parseTimeFromValue(value)
@@ -34,6 +65,31 @@ timeInput <- function(inputId, label, value = NULL) {
   )
 }
 
+#' Change the value of a time input on the client
+#'
+#' @inheritParams shiny::updateTextInput
+#' @param value The desired time value. Must be a instance of \code{\link{DateTimeClasses}}.
+#'
+#' @family shinyTime functions
+#'
+#' @examples
+#' ## Only run examples in interactive R sessions
+#' if (interactive()) {
+#'
+#' ui <- fluidPage(
+#'   timeInput("time", "Time:"),
+#'   actionButton("to_current_time", "Current time")
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   observeEvent(input$to_current_time, {
+#'     updateTimeInput(session, "time", value = Sys.time())
+#'   })
+#' }
+#'
+#' shinyApp(ui, server)
+#' }
+#'
 #' @export
 updateTimeInput <- function(session, inputId, label = NULL, value = NULL) {
   value <- parseTimeFromValue(value)
@@ -45,7 +101,7 @@ updateTimeInput <- function(session, inputId, label = NULL, value = NULL) {
 #'
 #' This functions runs a very simple app demonstrating the shinyTime functionality.
 #'
-#' @family shinyTime
+#' @family shinyTime functions
 #' @importFrom shiny runApp
 #' @export
 shinyTimeExample <- function() {
