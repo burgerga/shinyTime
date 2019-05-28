3d7d58d90d83d685b850bdddcf9b246610577c1f
Gerhard Burger <burger.ga@gmail.com>
2019-05-28 11:20:14 +0200

    Change second time render text to `hh:mm`

--- inst/example/app.R
+++ inst/example/app.R
@@ -29,7 +29,7 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$time_output1 <- renderText(strftime(input$time_input1, "%T"))
-  output$time_output2 <- renderText(strftime(input$time_input2, "%T"))
+  output$time_output2 <- renderText(strftime(input$time_input2, "%R"))
 
   observeEvent(input$to_current_time, {
     updateTimeInput(session, "time_input1", value = Sys.time())
